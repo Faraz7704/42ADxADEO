f9e9a185ed205b53d6d5b1c834c5966d749b3300
Rashidov01 <avazbekrashidov6@gmail.com>
2024-11-18 22:00:23 +0400

    answer page and request page tables is done

--- frontend/app/home/answers/page.tsx
+++ frontend/app/home/answers/page.tsx
@@ -1,11 +1,10 @@
+"use client";
+import UserQuestionsTable from "@/components/request-table";
+
 export default function ProfilePage() {
-    return (
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="aspect-video rounded-xl bg-neutral-100/50 dark:bg-neutral-800/50" />
-        <div className="aspect-video rounded-xl bg-neutral-100/50 dark:bg-neutral-800/50" />
-        <div className="aspect-video rounded-xl bg-neutral-100/50 dark:bg-neutral-800/50" />
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-neutral-100/50 md:min-h-min dark:bg-neutral-800/50" />
-      </div>
-    )
-  }
-  
+  return (
+    <div>
+      <UserQuestionsTable />
+    </div>
+  );
+}
