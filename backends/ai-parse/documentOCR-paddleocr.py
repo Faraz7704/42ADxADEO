1ebe9bbae9fc42edb017b9a0d1ed958a9c245f86
jyao <jinxiuyao@gmail.com>
2024-11-18 12:50:02 +0000

    complete models for OCR and documentQA

---
+++ backends/ai-parse/documentOCR-paddleocr.py
@@ -0,0 +1,23 @@
+from paddleocr import PaddleOCR,draw_ocr
+# Paddleocr supports Chinese, English, French, German, Korean and Japanese.
+# You can set the parameter `lang` as `ch`, `en`, `fr`, `german`, `korean`, `japan`
+# to switch the language model in order.
+ocr = PaddleOCR(use_angle_cls=True, lang='ar', use_gpu=0) # need to run only once to download and load model into memory
+img_path = 'arabic_example.png'
+result = ocr.ocr(img_path, cls=True)
+for idx in range(len(result)):
+    res = result[idx]
+    for line in res:
+        print(line)
+
+
+# draw result
+from PIL import Image
+result = result[0]
+image = Image.open(img_path).convert('RGB')
+boxes = [line[0] for line in result]
+txts = [line[1][0] for line in result]
+scores = [line[1][1] for line in result]
+im_show = draw_ocr(image, boxes, txts, scores, font_path='/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf')
+im_show = Image.fromarray(im_show)
+im_show.save('result.jpg')
---
+++ backends/ai-parse/documentQA-blip-image-captioning-large.py
@@ -0,0 +1,51 @@
+import requests
+import torch
+from PIL import Image
+from transformers import MllamaForConditionalGeneration, AutoProcessor
+
+model_id = "meta-llama/Llama-3.2-11B-Vision-Instruct"
+
+model = MllamaForConditionalGeneration.from_pretrained(
+    model_id,
+    torch_dtype=torch.bfloat16,
+    device_map="cuda:0",
+)
+processor = AutoProcessor.from_pretrained(model_id)
+
+# url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/0052a70beed5bf71b92610a43a52df6d286cd5f3/diffusers/rabbit.jpg"
+url = "https://amzirlodp-prd-s3.s3.amazonaws.com/documents/images/big_d69ffc0317c5d488605bff70bc85e9019474f0c8.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+
+messages = [
+    {"role": "user", "content": [
+        {"type": "image"},
+        {"type": "text", "text": "Tell me what is in the photo."}
+    ]}
+]
+input_text = processor.apply_chat_template(messages, add_generation_prompt=True)
+inputs = processor(
+    image,
+    input_text,
+    add_special_tokens=False,
+    return_tensors="pt"
+).to(model.device)
+
+output = model.generate(**inputs, max_new_tokens=100)
+print(processor.decode(output[0]))
+
+messages = [
+    {"role": "user", "content": [
+        {"type": "image"},
+        {"type": "text", "text": "Tell me what is in the photo in detail."}
+    ]}
+]
+input_text = processor.apply_chat_template(messages, add_generation_prompt=True)
+inputs = processor(
+    image,
+    input_text,
+    add_special_tokens=False,
+    return_tensors="pt"
+).to(model.device)
+
+output = model.generate(**inputs, max_new_tokens=1000)
+print(processor.decode(output[0]))
